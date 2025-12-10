afc076d21003392a1d17bc19b9baff8d14e1d627
Song YoungJu <moeirs545@gmail.com>
2025-12-10 14:32:21 +0900

    ✨ Common - SearchInput, TextInput 컴포넌트 작성

---
+++ src/components/common/SearchInput.tsx
@@ -0,0 +1,49 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { Search } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+
+const searchInput = cva(
+  "bg-bg-primary text-content-primary inline-flex items-center gap-3 rounded-full px-5 py-3",
+  {
+    variants: {
+      inputSize: {
+        md: "h-10 min-w-75 text-sm",
+        lg: "h-17.5 min-w-[525px] text-base",
+      },
+    },
+    defaultVariants: {
+      inputSize: "md",
+    },
+  },
+);
+
+type SearchInputTypes = "md" | "lg";
+
+interface SearchInputProps
+  extends
+    React.ComponentPropsWithoutRef<"input">,
+    VariantProps<typeof searchInput> {
+  inputSize: SearchInputTypes;
+  placeholder: string;
+  className?: string;
+}
+
+export default function SearchInput({
+  inputSize,
+  placeholder,
+  className,
+}: SearchInputProps) {
+  return (
+    <div className={twMerge(searchInput({ inputSize }), className)}>
+      <Search
+        size={inputSize === "md" ? 16 : 25}
+        className="text-content-secondary"
+      />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="placeholder:text-content-tertiary w-full py-2 outline-0"
+      />
+    </div>
+  );
+}
---
+++ src/components/common/TextInput.tsx
@@ -0,0 +1,23 @@
+import { twMerge } from "tailwind-merge";
+
+interface TextInputProps extends React.ComponentPropsWithoutRef<"input"> {
+  placeholder: string;
+  className?: string;
+}
+
+export default function TextInput({ placeholder, className }: TextInputProps) {
+  return (
+    <div
+      className={twMerge(
+        "bg-bg-primary placeholder:text-content-tertiary text-content-primary inline-flex h-15 min-w-125 items-center gap-3 rounded-xl px-5 py-5",
+        className,
+      )}
+    >
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="placeholder:text-content-tertiary w-full py-2 text-lg outline-0"
+      />
+    </div>
+  );
+}
