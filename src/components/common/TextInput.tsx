eaf43fe683dacaccc872b6448a4b3674aa776e70
Song YoungJu <moeirs545@gmail.com>
2025-12-11 16:59:50 +0900

    ♻️ TextInput width 수정 (부모 크기에 맞게)

--- src/components/common/TextInput.tsx
+++ src/components/common/TextInput.tsx
@@ -9,7 +9,7 @@ export default function TextInput({ placeholder, className }: TextInputProps) {
   return (
     <div
       className={twMerge(
-        "bg-bg-primary placeholder:text-content-tertiary text-content-primary inline-flex h-15 min-w-125 items-center gap-3 rounded-xl px-5 py-5",
+        "bg-bg-primary placeholder:text-content-tertiary text-content-primary flex h-15 items-center gap-3 rounded-xl px-5 py-5",
         className,
       )}
     >
