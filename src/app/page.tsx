eace61f5177c5de092a00c91fc077d7f62118351
SangYoonLee1231 <sylagape1231@gmail.com>
2025-12-11 14:47:50 +0900

    🚑 홈페이지 원상복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-"use client";
-import { useState } from "react";
-import ToggleBtn, { RecruitStatus } from "@/components/common/button/ToggleBtn";
-
 export default function Home() {
-  const [status, setStatus] = useState<RecruitStatus>("completed");
-
-  return (
-    <>
-      <ToggleBtn value={status} onChange={setStatus} />
-    </>
-  );
+  return <h1>Home</h1>;
 }
