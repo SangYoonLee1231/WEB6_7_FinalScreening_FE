12ca21c4089c854fafeebe1a8f2c36af49edd327
Song YoungJu <moeirs545@gmail.com>
2025-12-11 16:38:19 +0900

    ✨ 모집 카드 티어 영역 구현

--- src/components/profile/TierSet.tsx
+++ src/components/profile/TierSet.tsx
@@ -1,18 +1,35 @@
 import Image from "next/image";
-import tierImage from "../../assets/images/tiers/Diamond.png";
 import { Rank, Tier, tierIcons } from "@/types/tier";
+import { twMerge } from "tailwind-merge";
+import { romanToNumber } from "@/utils/romanToNumber";
 
+type TierSetType = "default" | "mini";
 interface TierSetProps {
-  tier: Tier;
-  rank: Rank;
+  tier: Tier | "UNRANKED";
+  rank: Rank | "";
+  type: TierSetType;
+  className?: string;
 }
 
-export default function TierSet({ tier, rank }: TierSetProps) {
+export default function TierSet({
+  tier,
+  rank,
+  type = "default",
+  className,
+}: TierSetProps) {
   return (
-    <div className="flex w-32.5 flex-col items-center justify-center">
+    <div
+      className={twMerge(
+        "flex w-32.5 flex-col items-center justify-center text-sm",
+        type === "mini" && "w-12.5",
+        className,
+      )}
+    >
       <Image src={tierIcons[tier]} alt={`${tier} tier image`} />
-      <span className="text-content-secondary text-sm">
-        {tier} {rank}
+      <span className="text-content-secondary">
+        {type === "default"
+          ? `${tier} ${rank}`
+          : `${tier[0]}${romanToNumber(rank)}`}
       </span>
     </div>
   );
