24576720053a88b29f408aa7ddbfac96aad94448
Song YoungJu <moeirs545@gmail.com>
2025-12-11 11:41:30 +0900

    ✨ 최근 선호, 챔피언 숙련도 type 추가

--- src/components/profile/MostChampion.tsx
+++ src/components/profile/MostChampion.tsx
@@ -28,32 +28,51 @@ const title = cva("font-semibold", {
 });
 
 const champions = [
-  { id: 0, src: Champion.src },
-  { id: 1, src: Champion.src },
-  { id: 2, src: Champion.src },
+  { id: 0, src: Champion.src, percent: 50 },
+  { id: 1, src: Champion.src, percent: 50 },
+  { id: 2, src: Champion.src, percent: 50 },
 ];
 
+type MostChampionType = "recent" | "mastery";
+
 interface MostChampionProps extends VariantProps<typeof container> {
-  data: { id: number; src: string }[]; // 데이터 확정되면 변경
+  data: { id: number; src: string; percent: number }[]; // 데이터 확정되면 변경
+  type: MostChampionType;
   className?: string;
 }
 
 export default function MostChampion({
-  size,
+  size = "sm",
+  type,
   data = champions,
   className,
 }: MostChampionProps) {
   return (
     <div className={twMerge(container({ size }), className)}>
-      <p className={twMerge(title({ size }), className)}>최근 선호 챔피언</p>
+      <p className={twMerge(title({ size }), className)}>
+        {type === "recent" ? "최근 선호 챔피언" : "챔피언 숙련도 TOP 3"}
+      </p>
       <div className="flex items-center justify-between">
-        {champions.map((champ) => (
-          <Avatar
-            key={champ.id}
-            type="champion"
-            src={Champion.src}
-            size={size === "sm" ? "sm" : "lg"}
-          />
+        {data.map((champ) => (
+          <div className="relative flex">
+            {" "}
+            <Avatar
+              key={champ.id}
+              type="champion"
+              src={Champion.src}
+              size={size === "sm" ? "sm" : "lg"}
+            />
+            <div className="bg-bg-tertiary absolute right-0 bottom-0 flex items-center justify-center">
+              <span
+                className={twMerge(
+                  "text-accent px-1 py-0.5 text-[8px]",
+                  size === "lg" && "px-1.5 text-[11px]",
+                )}
+              >
+                {champ.percent}%
+              </span>
+            </div>
+          </div>
         ))}
       </div>
     </div>
