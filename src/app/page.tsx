bb3a4cf85c552aea3a75f1f872f67c9e5ecbad36
SangYoonLee1231 <sylagape1231@gmail.com>
2025-12-12 11:33:51 +0900

    🚑 page 원상태로 복귀

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-import ReviewCard from "@/components/review/ReviewCard";
-
 export default function Home() {
-  return (
-    <>
-      // 받은 리뷰 리스트
-      <ReviewCard
-        mode="received"
-        gameName="lol"
-        communityName="커뮤니티 닉네임"
-        content="리뷰내용"
-        emotion="good"
-        createdAt="2025-12-12T00:12:00.000Z"
-      />
-      // 작성한 리뷰 리스트
-      <ReviewCard
-        mode="written"
-        gameName="lol"
-        communityName="커뮤니티 닉네임"
-        content="리뷰내용"
-        emotion="bad"
-        createdAt="2025-12-11T00:11:00.000Z"
-      />
-    </>
-  );
+  return <h1>Home</h1>;
 }
