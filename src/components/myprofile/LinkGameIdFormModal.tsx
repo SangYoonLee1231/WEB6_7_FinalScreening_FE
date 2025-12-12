177b677fd816832cae9c1db52e4a9351d5517826
suyeon <suyeon1104@gmail.com>
2025-12-12 14:18:18 +0900

    ➖ ChevronDown import 제거

--- src/components/myprofile/LinkGameIdFormModal.tsx
+++ src/components/myprofile/LinkGameIdFormModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { CircleAlert, ChevronDown } from "lucide-react";
+import { CircleAlert } from "lucide-react";
 import TextInput from "@/components/common/TextInput";
 import Dropdown from "@/components/common/Dropdown";
 import { BoxButton } from "@/components/common/button/BoxButton";
