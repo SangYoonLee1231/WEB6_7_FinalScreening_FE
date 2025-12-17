fe23904c72b620d81f8a506db16b823803046f26
suyeon <suyeon1104@gmail.com>
2025-12-17 16:40:01 +0900

    ♻️ (protected, public) 그룹 layout 수정

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,11 +2,11 @@ import Header from "@/components/common/Nav/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="flex h-dvh flex-col">
       <Header type="full" />
-      <div className="m-auto min-h-dvh w-(--content-area) max-w-full">
-        <main>{children}</main>
+      <div className="m-auto w-(--content-area) max-w-full flex-1">
+        <main className="h-full w-full">{children}</main>
       </div>
-    </>
+    </div>
   );
 }
