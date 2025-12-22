4ff24e5b3d95f3d16c400aa75adde6a0dc461ac6
Song YoungJu <moeirs545@gmail.com>
2025-12-22 17:24:27 +0900

    📦️ Tanstack query devtools 설치

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [client] = useState(() => new QueryClient());
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {" "}
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
 }
