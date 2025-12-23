1fb5d18b50f4a7eb5cb9d88bc44f5403cd8a60aa
Song YoungJu <moeirs545@gmail.com>
2025-12-23 09:39:39 +0900

    ✨ Error, Loading 페이지 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingBouncy from "@/components/common/loading/LoadingBouncy";
+
+export default function loading() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <LoadingBouncy />
+    </div>
+  );
+}
