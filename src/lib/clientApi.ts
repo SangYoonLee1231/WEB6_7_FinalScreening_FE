18c5204c7050b9d9b353dfffd05b25e8a48aab21
SangYoonLee1231 <sylagape1231@gmail.com>
2025-12-24 11:17:27 +0900

    ✨ 체팅 목록 스크롤 관련 개발

--- src/lib/clientApi.ts
+++ src/lib/clientApi.ts
@@ -1,5 +1,5 @@
 export const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8080";
+  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080";
 
 export default async function ClientApi(path: string, init: RequestInit) {
   return await fetch(`${API_BASE}${path}`, {
