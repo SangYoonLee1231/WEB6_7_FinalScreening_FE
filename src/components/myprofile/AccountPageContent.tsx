a505516ad9e3b4f260c40a477f4d661a2990e51b
suyeon <suyeon1104@gmail.com>
2025-12-27 23:18:24 +0900

    ♻️ 컴포넌트 분리 (닉네임, 프로필 이미지, 소개, 비밀번호)

--- src/components/myprofile/AccountPageContent.tsx
+++ src/components/myprofile/AccountPageContent.tsx
@@ -16,7 +16,7 @@ import {
   useState,
   useTransition,
 } from "react";
-import ClientApi, { API_BASE } from "@/lib/clientApi";
+import ClientApi from "@/lib/clientApi";
 
 interface profileDataProps {
   email: string;
@@ -46,66 +46,92 @@ export default function AccountPageContent() {
   const { setMenu } = useMenuStore();
   const { setMenu: setProfileMenu } = useMyProfileMenuStore();
 
-  const [nickname, setNickname] = useState<string>("");
-  const [tempNickname, setTempNickname] = useState<string>("");
-  const [isNicknameEditing, setIsNicknameEditing] = useState<boolean>(false);
-  const [optimisticNickname, addOptimisticNickname] = useOptimistic<
-    string | null,
-    string
-  >(nickname, (_: string | null, nextValue: string) => nextValue);
-  const [isNicknamePending, startNicknameTransition] = useTransition();
-  const [nicknameError, setNicknameError] = useState<string | null>(null);
-
-  const [email, setEmail] = useState<string>("");
-
-  const [profileImage, setProfileImage] = useState<string | null>(null);
-  const [optimisticProfileImage, addOptimisticProfileImage] = useOptimistic<
-    string | null,
-    string
-  >(profileImage, (_: string | null, nextValue: string) => nextValue);
-  const [isImagePending, startImageTransition] = useTransition();
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const [comment, setComment] = useState<string>("");
-  const [tempComment, setTempComment] = useState<string>("");
-  const [isCommentEditing, setIsCommentEditing] = useState<boolean>(false);
-
-  const [password, setPassword] = useState<string>("");
-  const [newPassword, setNewPassword] = useState<string>("");
-  const [newPasswordConfirm, setNewPasswordConfirm] = useState<string>("");
-  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [profileData, setProfileData] = useState<profileDataProps>({
+    email: "",
+    profile_image: null,
+    nickname: "",
+    comment: "",
+  });
 
   useEffect(() => {
     setMenu("profile");
     setProfileMenu("account");
+
     const fetchMyProfile = async () => {
-      const res = await ClientApi(`/api/v1/users/me`, {
-        method: "GET",
-      });
-
-      const profileData: profileDataProps = await res.json();
-
-      if (res.ok) {
-        // console.log("profileData", profileData);
-        setNickname(profileData.nickname);
-        setProfileImage(profileData.profile_image);
-        setEmail(profileData.email);
-        setComment(profileData.comment ?? "");
+      try {
+        const res = await ClientApi("/api/v1/users/me", {
+          method: "GET",
+        });
+        if (!res.ok) {
+          throw new Error("서버 응답 에러");
+        }
+        const data = await res.json();
+        setProfileData(data);
+        console.log("set profileData", data);
+      } catch (error) {
+        if (error instanceof Error) {
+          alert(error.message);
+        } else {
+          alert("알 수 없는 오류가 발생했습니다.");
+        }
       }
     };
 
     fetchMyProfile();
-  }, []);
+  }, [setMenu, setProfileMenu]);
 
   // useEffect(() => {
   //   console.log("profileImage", profileImage)
   // }, [profileImage]);
 
+
+
+  return (
+    <div className="flex w-125 flex-col gap-11 [&_h3]:text-xl [&_h3]:font-semibold">
+      <h2 className="text-4xl font-bold">계정 관리</h2>
+      <div className="flex flex-col gap-9">
+        <div className="flex items-center gap-10">
+          <ProfileImageSection initialProfileImage={profileData.profile_image ?? ""}/>
+          <NicknameSection initialNickname={profileData.nickname} />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h3>이메일</h3>
+          <span>{profileData.email}</span>
+        </div>
+
+        <CommentSection initialComment={profileData.comment ?? ""} />
+        <PasswordSection />
+      </div>
+    </div>
+  );
+}
+
+interface NicknameProps {
+  initialNickname: string;
+}
+
+function NicknameSection({ initialNickname }: NicknameProps) {
+  const [nickname, setNickname] = useState<string>("");
+  const [modifyNickname, setModifyNickname] = useState<string>("");
+  const [isNicknameEditing, setIsNicknameEditing] = useState<boolean>(false);
+  const [nicknameError, setNicknameError] = useState<string | null>(null);
+
+  const [isNicknamePending, startNicknameTransition] = useTransition();
+  const [optimisticNickname, addOptimisticNickname] = useOptimistic<
+    string | null,
+    string
+  >(nickname, (_: string | null, nextValue: string) => nextValue);
+
+  useEffect(() => {
+    setNickname(initialNickname);
+  }, [initialNickname]);
+
   const handleNicknameSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setNicknameError(null);
     if (isNicknamePending) return;
-    const parsedNickname = nicknameSchema.safeParse(tempNickname);
+    const parsedNickname = nicknameSchema.safeParse(modifyNickname);
 
     if (!parsedNickname.success) {
       setNicknameError(parsedNickname.error.issues[0]?.message);
@@ -121,7 +147,7 @@ export default function AccountPageContent() {
         });
 
         if (res.ok) {
-          setNickname(tempNickname);
+          setNickname(modifyNickname);
           setIsNicknameEditing(false);
         } else {
           alert("닉네임 변경에 실패했습니다.");
@@ -132,9 +158,77 @@ export default function AccountPageContent() {
     });
   };
 
-  const handleEditClick = () => {
-    fileInputRef?.current?.click();
-  };
+  return (
+    <div className="space-y-4">
+      <h3>닉네임</h3>
+      <div className="space-y-2">
+        {isNicknameEditing ? (
+          <form
+            className="flex flex-row items-center space-x-4"
+            onSubmit={handleNicknameSubmit}
+          >
+            <TextInput
+              value={modifyNickname}
+              onChange={(e) => setModifyNickname(e.target.value)}
+              placeholder={nickname ?? ""}
+              className="h-5 w-42 py-4"
+            />
+
+            <button
+              type="submit"
+              className="text-accent cursor-pointer hover:underline"
+            >
+              {isNicknamePending ? "저장 중..." : "저장"}
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setIsNicknameEditing(false);
+                setNicknameError(null);
+              }}
+              className="text-content-secondary cursor-pointer hover:underline"
+            >
+              취소
+            </button>
+          </form>
+        ) : (
+          <div className="flex flex-row space-x-4">
+            <span>{optimisticNickname}</span>
+            <button
+              className="text-accent cursor-pointer hover:underline"
+              onClick={() => {
+                console.log("optimisticNickname", optimisticNickname);
+                setModifyNickname(nickname ?? "");
+                setIsNicknameEditing(true);
+              }}
+            >
+              수정
+            </button>
+          </div>
+        )}
+        {nicknameError && (
+          <p className="text-negative ml-2 text-sm">{nicknameError}</p>
+        )}
+        <div className="text-content-secondary flex items-center gap-2 text-base">
+          <CircleAlert size={18} />
+          <p>닉네임은 7일 단위로 변경할 수 있습니다</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+interface CommentProps {
+  initialComment: string;
+}
+
+function CommentSection({ initialComment }: CommentProps) {
+  const [comment, setComment] = useState<string>("");
+  const [tempComment, setTempComment] = useState<string>("");
+  const [isCommentEditing, setIsCommentEditing] = useState<boolean>(false);
+
+  useEffect(() => {
+    setComment(initialComment);
+  });
 
   const handleCommentSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -154,6 +248,63 @@ export default function AccountPageContent() {
       alert("서버 통신 중 오류가 발생했습니다.");
     }
   };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h3>소개</h3>
+      {isCommentEditing ? (
+        <form
+          className="flex flex-row items-center space-x-4"
+          onSubmit={handleCommentSubmit}
+        >
+          <TextInput
+            value={tempComment}
+            onChange={(e) => setTempComment(e.target.value)}
+            placeholder={comment}
+            className="h-11 w-90 py-4"
+          />
+
+          <button
+            type="submit"
+            className="text-accent cursor-pointer hover:underline"
+          >
+            저장
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setIsCommentEditing(false);
+              setTempComment(comment);
+            }}
+            className="text-content-secondary cursor-pointer hover:underline"
+          >
+            취소
+          </button>
+        </form>
+      ) : (
+        <div className="flex flex-row justify-items-center space-x-4">
+          <IntroduceBubble content={comment ?? ""} type="message" />
+          <BoxButton
+            text="수정"
+            tone="color"
+            size="xs"
+            className="flex-nowrap self-end"
+            onClick={() => {
+              setTempComment(comment);
+              setIsCommentEditing(true);
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function PasswordSection() {
+  const [password, setPassword] = useState<string>("");
+  const [newPassword, setNewPassword] = useState<string>("");
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string | null>(null);
 
   const handlePasswordSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -186,6 +337,52 @@ export default function AccountPageContent() {
     } catch (error) {
       alert("서버 통신 중 오류가 발생했습니다.");
     }
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h3>비밀번호 변경</h3>
+      <form className="flex flex-col gap-2">
+        <TextInput
+          placeholder="현재 비밀번호"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <TextInput
+          placeholder="새 비밀번호"
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+        <TextInput
+          placeholder="새 비밀번호 확인"
+          onChange={(e) => setNewPasswordConfirm(e.target.value)}
+        />
+        <span className="ml-2">{passwordError}</span>
+        <BoxButton
+          text="수정"
+          tone="color"
+          size="xs"
+          className="mt-3 flex-nowrap self-end"
+          onClick={handlePasswordSubmit}
+        />
+      </form>
+    </div>
+  );
+}
+
+interface ProfileImageProps {
+  initialProfileImage: string;
+}
+
+function ProfileImageSection(initialProfileImage: ProfileImageProps) {
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+  const [optimisticProfileImage, addOptimisticProfileImage] = useOptimistic<
+    string | null,
+    string
+  >(profileImage, (_: string | null, nextValue: string) => nextValue);
+  const [isImagePending, startImageTransition] = useTransition();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleEditClick = () => {
+    fileInputRef?.current?.click();
   };
 
   const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
@@ -241,167 +438,23 @@ export default function AccountPageContent() {
   };
 
   return (
-    <div className="flex w-125 flex-col gap-11 [&_h3]:text-xl [&_h3]:font-semibold">
-      <h2 className="text-4xl font-bold">계정 관리</h2>
-      <div className="flex flex-col gap-9">
-        <div className="flex items-center gap-10">
-          <div className="relative">
-            <Avatar
-              size="xl"
-              src={optimisticProfileImage ?? ""}
-              type="profile"
-            />
-            <CircleBtn
-              size="xs"
-              className="bg-accent absolute right-0 bottom-0"
-              onClick={handleEditClick}
-              disabled={isImagePending}
-            >
-              <Pencil size={15} strokeWidth={3} />
-            </CircleBtn>
-            <input
-              type="file"
-              ref={fileInputRef}
-              onChange={handleFileChange}
-              accept="image/*"
-              style={{ display: "none" }}
-            />
-          </div>
-          <div className="space-y-4">
-            <h3>닉네임</h3>
-            <div className="space-y-2">
-              {isNicknameEditing ? (
-                <form
-                  className="flex flex-row items-center space-x-4"
-                  onSubmit={handleNicknameSubmit}
-                >
-                  <TextInput
-                    value={tempNickname}
-                    onChange={(e) => setTempNickname(e.target.value)}
-                    placeholder={nickname}
-                    className="h-5 w-42 py-4"
-                  />
-
-                  <button
-                    type="submit"
-                    className="text-accent cursor-pointer hover:underline"
-                  >
-                    {isNicknamePending ? "저장 중..." : "저장"}
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      setIsNicknameEditing(false);
-                      setTempNickname(nickname);
-                      setNicknameError(null);
-                    }}
-                    className="text-content-secondary cursor-pointer hover:underline"
-                  >
-                    취소
-                  </button>
-                </form>
-              ) : (
-                <div className="flex flex-row space-x-4">
-                  <span>{optimisticNickname}</span>
-                  <button
-                    className="text-accent cursor-pointer hover:underline"
-                    onClick={() => {
-                      setTempNickname(nickname);
-                      setIsNicknameEditing(true);
-                    }}
-                  >
-                    수정
-                  </button>
-                </div>
-              )}
-              {/* 에러 메시지 표시 */}
-              {nicknameError && (
-                <p className="text-negative ml-2 text-sm">{nicknameError}</p>
-              )}
-              <div className="text-content-secondary flex items-center gap-2 text-base">
-                <CircleAlert size={18} />
-                <p>닉네임은 7일 단위로 변경할 수 있습니다</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <h3>소개</h3>
-          {isCommentEditing ? (
-            <form
-              className="flex flex-row items-center space-x-4"
-              onSubmit={handleCommentSubmit}
-            >
-              <TextInput
-                value={tempComment}
-                onChange={(e) => setTempComment(e.target.value)}
-                placeholder={comment}
-                className="h-11 w-90 py-4"
-              />
-
-              <button
-                type="submit"
-                className="text-accent cursor-pointer hover:underline"
-              >
-                저장
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  setIsCommentEditing(false);
-                  setTempComment(comment);
-                }}
-                className="text-content-secondary cursor-pointer hover:underline"
-              >
-                취소
-              </button>
-            </form>
-          ) : (
-            <div className="flex flex-row justify-items-center space-x-4">
-              <IntroduceBubble content={comment ?? ""} type="message" />
-              <BoxButton
-                text="수정"
-                tone="color"
-                size="xs"
-                className="flex-nowrap self-end"
-                onClick={() => {
-                  setTempComment(comment);
-                  setIsCommentEditing(true);
-                }}
-              />
-            </div>
-          )}
-        </div>
-        <div className="flex flex-col gap-2">
-          <h3>이메일</h3>
-          <span>{email}</span>
-        </div>
-        <div className="flex flex-col gap-2">
-          <h3>비밀번호 변경</h3>
-          <form className="flex flex-col gap-2">
-            <TextInput
-              placeholder="현재 비밀번호"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <TextInput
-              placeholder="새 비밀번호"
-              onChange={(e) => setNewPassword(e.target.value)}
-            />
-            <TextInput
-              placeholder="새 비밀번호 확인"
-              onChange={(e) => setNewPasswordConfirm(e.target.value)}
-            />
-            <span className="ml-2">{passwordError}</span>
-            <BoxButton
-              text="수정"
-              tone="color"
-              size="xs"
-              className="mt-3 flex-nowrap self-end"
-              onClick={handlePasswordSubmit}
-            />
-          </form>
-        </div>
-      </div>
+    <div className="relative">
+      <Avatar size="xl" src={optimisticProfileImage ?? ""} type="profile" />
+      <CircleBtn
+        size="xs"
+        className="bg-accent absolute right-0 bottom-0"
+        onClick={handleEditClick}
+        disabled={isImagePending}
+      >
+        <Pencil size={15} strokeWidth={3} />
+      </CircleBtn>
+      <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        accept="image/*"
+        style={{ display: "none" }}
+      />
     </div>
   );
 }
