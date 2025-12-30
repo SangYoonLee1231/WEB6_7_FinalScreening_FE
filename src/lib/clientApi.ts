34bb6c40da7dcd4209032975153a56694375ce1a
SangYoonLee1231 <sylagape1231@gmail.com>
2025-12-30 21:23:35 +0900

    Merge

--- src/lib/clientApi.ts
+++ src/lib/clientApi.ts
@@ -2,7 +2,7 @@ export const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 
 export default async function ClientApi(path: string, init: RequestInit) {
   // base 끝의 / 제거
-  const base = API_BASE.replace(/\/+$/, "");
+  const base = (API_BASE || "").replace(/\/+$/, "");
   // path 앞에 / 보장
   const normalizedPath = path.startsWith("/") ? path : `/${path}`;
 
